fbe54b8ab37633d2c524a2b1bef3a499e06f3398
David Díaz <58749180+davidadtorres@users.noreply.github.com>
2021-06-19 00:39:48 +0100

    chore: update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+#
+# commit.sh
+#
+# Syntax:
+#   commit.sh 'commit_message' [-r]
+#   -r: create a tag release
+#
+
+if [ $# -gt 2 ] || [ $# -lt 1 ]
+then
+  echo -e "SYNTAX ERROR: parameter expected!\n\ncommit.sh 'commit_message' [-r]\n  -r: create a tag release"
+  exit -1
+fi
 
 build=$(grep "BUILD=" .env | cut -d= -f2)
 ((build += 1))
@@ -9,9 +22,13 @@ git commit -m "$1"
 
 if [ $? == '0' ]
 then
-   npm run release
+   if [ $# -eq 2 ] && [ $2 == '-r' ]
+   then    
+     npm run release
+   fi
    git push --follow-tags origin-git master
 else
-   ((build -= 1))
-   sed -i "s/BUILD=[0-9]\+/BUILD=$build/g" .env
+  ((build -= 1))
+  sed -i "s/BUILD=[0-9]\+/BUILD=$build/g" .env
 fi
+git log -n 4 --oneline

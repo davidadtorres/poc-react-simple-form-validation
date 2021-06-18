48fb64def09ecf43f4942d468d107d6d6f79877f
David Alberto Díaz Torres <davidadtorres@gmail.com>
2021-06-18 23:48:46 +0100

    chore: update commit.sh

--- commit.sh
+++ commit.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#
+# commit.sh
+#
+# Syntax:
+#   -r: create a release
+#
+
+if [ $# -gt 1 ]
+then
+  echo -e "SYNTAX ERROR: parameter expected!\n  commit.sh [-r]"
+  exit -1
+fi
 
 build=$(grep "BUILD=" .env | cut -d= -f2)
 ((build += 1))
@@ -9,9 +21,12 @@ git commit -m "$1"
 
 if [ $? == '0' ]
 then
-   npm run release
+   if [ $# -eq 1 ] && [ $1 == '-r' ]
+   then    
+     npm run release
+   fi
    git push --follow-tags origin-git master
 else
-   ((build -= 1))
-   sed -i "s/BUILD=[0-9]\+/BUILD=$build/g" .env
+  ((build -= 1))
+  sed -i "s/BUILD=[0-9]\+/BUILD=$build/g" .env
 fi

c58f35dbf7dc33177a6efe53fdfbb6f1c8d93878
David Alberto Díaz Torres <davidadtorres@gmail.com>
2021-06-19 00:21:10 +0100

    docs: update commit.sh

--- commit.sh
+++ commit.sh
@@ -4,12 +4,12 @@
 #
 # Syntax:
 #   commit.sh "commit_message" [-r]
-#   -r: create a release
+#   -r: create a tag release
 #
 
-if [ $# -gt 2 ]
+if [ $# -gt 2 ] || [ $# -lt 1 ]
 then
-  echo -e "SYNTAX ERROR: parameter expected!\n\ncommit.sh \"commit_message\" [-r]\n  -r: create a release"
+  echo -e "SYNTAX ERROR: parameter expected!\n\ncommit.sh \"commit_message\" [-r]\n  -r: create a tag release"
   exit -1
 fi
 
@@ -22,10 +22,13 @@ git commit -m "$1"
 
 if [ $? == '0' ]
 then
-   npm run release
+   if [ $# -eq 2 ] && [ $2 == '-r' ]
+   then    
+     npm run release
+   fi
    git push --follow-tags origin-git master
 else
   ((build -= 1))
   sed -i "s/BUILD=[0-9]\+/BUILD=$build/g" .env
 fi
-git log -n 4 --oneline
+git log -n 3 --oneline
